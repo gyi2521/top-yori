ff336b8a7d53ee55a990f6c49d4f0cbb83158529
gyi <ginay2521@gmail.com>
2019-02-05 14:16:53 -0500

    yarn build

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -2,7 +2,8 @@ import React from "react";
 
 const HeaderBar = () => (
   <div className="headerBar">
-    <a href="#" className="headerName">TopYori</a>
+    <a href="#" className="headerName">Top Yori </a>
+    <a className='headerDetail'> - find popular dish in your area</a>
     <a href="#" className="headerLogin">User Login</a>
   </div>
 );
--- server.js
+++ server.js
@@ -17,9 +17,12 @@ if (process.env.NODE_ENV === "production") {
 require("./routes/api-routes")(app);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactDishApp");
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactDishApp");
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password12@ds141043.mlab.com:41043/heroku_zzcz8hmq")
+//mongoose.connect('mongodb://gyi:NamJee01@ds121282.mlab.com:21282/heroku_js7qh2wp', {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://gina:NamJee1@ds141043.mlab.com:41043/heroku_zzcz8hmq");
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
